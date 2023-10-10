2412320f505b6c6fc67eea95add31fe9eb7e0036
DWIPAYAN ROY <145047274+ROYDWIPS@users.noreply.github.com>
2023-10-10 13:07:08 +0530

    Q-1.C

---
+++ Semester-1/Q-1.C
@@ -0,0 +1,26 @@
+//Write a program to check if the given number is prime or not//
+
+#include<stdio.h>
+void main()
+
+{ 
+int n,i,a=0;
+printf("Enter an integer number");
+scanf("%d",&n);
+for (i=2;i<=n/2;i++)
+{
+if(n%i==0)
+{
+a=1;
+break;
+}
+}
+if (a==0)
+{
+printf("%d is a prime number",n);
+}
+else
+{
+printf("%d is not a prime number",n);
+}
+}
