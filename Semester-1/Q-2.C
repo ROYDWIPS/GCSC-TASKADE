95b3627d263117e7c95dab2d76a4c947586e5349
DWIPAYAN ROY <145047274+ROYDWIPS@users.noreply.github.com>
2023-10-10 13:10:57 +0530

    Q-2.C

---
+++ Semester-1/Q-2.C
@@ -0,0 +1,12 @@
+//Write a program to print ASCII Value of a given character//
+
+
+#include<stdio.h>
+void main()
+
+{
+    char d;
+    printf("Enter a character");
+    scanf("%c",&d);
+    printf("ASCII value of %c = %d",d,d);
+}
